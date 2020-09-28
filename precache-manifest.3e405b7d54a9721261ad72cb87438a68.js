ffc640051d3ef8acf75e57d2a1088462cc9baaab
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-28 13:45:37 +0700

    Add files via upload

---
+++ precache-manifest.3e405b7d54a9721261ad72cb87438a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae2d3263329305d41b2adfac867c457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a72d3c526d71437bce1e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb6b885b244a9f13a20d",
+    "url": "/static/js/2.d69876ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d69876ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72d3c526d71437bce1e",
+    "url": "/static/js/main.35377502.chunk.js"
+  },
+  {
+    "revision": "7446b63276108d351200",
+    "url": "/static/js/runtime-main.ad615bf2.js"
+  }
+]);
